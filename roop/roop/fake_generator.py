7cdbfa04c70caf9832aafeb81e0b434f5c6a024a
KBC-1315 <tobe13155131@gmail.com>
2024-05-14 05:00:23 +0000

    make folder to store temp output

--- roop/roop/fake_generator.py
+++ roop/roop/fake_generator.py
@@ -3,7 +3,6 @@ import argparse
 import cv2
 
 import os
-import cv2
 import time
 
 def generate_unique_id() -> str:
@@ -17,7 +16,7 @@ def generate_fake(gender_type, source_image) :
     run_args = argparse.Namespace(
         source_path="/home/tobe1315/my_projects/Face_swapper/temp_source/{}".format(temp_id),
         target_path="/home/tobe1315/my_projects/Face_swapper/Background/Background/test.jpg",
-        output_path="/home/tobe1315/my_projects/Face_swapper/test_no.jpg",
+        output_path="/home/tobe1315/my_projects/Face_swapper/{}/test_no.jpg".format(temp_id),
         frame_processor=["face_swapper", "face_enhancer"],  # 필요한 경우 추가 파라미터를 설정할 수 있습니다
         keep_fps=False,
         keep_frames=False,
@@ -35,11 +34,14 @@ def generate_fake(gender_type, source_image) :
         execution_threads= 8 
     )
     run_args.headless = True  # headless 모드로 실행하도록 설정합니다
+    folder_path = r"/home/tobe1315/my_projects/Face_swapper/output/{}".format(temp_id.split(".")[0])
+    os.mkdir(folder_path)
+    
     if gender_type == "M" :
         male_list = []
         for i in range(1, 7):
             run_args.target_path = "/home/tobe1315/my_projects/Face_swapper/Background/Background/MALE_{}.jpg".format(i)
-            run_args.output_path = "/home/tobe1315/my_projects/Face_swapper/output/{}".format(temp_id.split(".")[0] + "_MALE_{}.jpg".format(i))
+            run_args.output_path = "/home/tobe1315/my_projects/Face_swapper/output/{}/{}".format(temp_id.split(".")[0],temp_id.split(".")[0] + "_MALE_{}.jpg".format(i))
             core.run(run_args)
             male_list.append(cv2.imread(run_args.output_path))
         return male_list
@@ -47,24 +49,23 @@ def generate_fake(gender_type, source_image) :
         female_list = []
         for i in range(1, 7):
             run_args.target_path = "/home/tobe1315/my_projects/Face_swapper/Background/Background/test_{}.jpg".format(i)
-            run_args.output_path = "/home/tobe1315/my_projects/Face_swapper/output/{}".format(temp_id.split(".")[0] + "_FEMALE_{}.jpg".format(i))
+            run_args.output_path = "/home/tobe1315/my_projects/Face_swapper/output/{}/{}".format(temp_id.split(".")[0],temp_id.split(".")[0] + "_FEMALE_{}.jpg".format(i))
             core.run(run_args)
             female_list.append(cv2.imread(run_args.output_path))
         return female_list
     else :
-        male_list = []
-        female_list = []
+        output_list = []
         for i in range(1, 7):
             run_args.target_path = "/home/tobe1315/my_projects/Face_swapper/Background/Background/MALE_{}.jpg".format(i)
-            run_args.output_path = "/home/tobe1315/my_projects/Face_swapper/output/{}".format(temp_id.split(".")[0] + "_MALE_{}.jpg".format(i))
+            run_args.output_path = "/home/tobe1315/my_projects/Face_swapper/output/{}/{}".format(temp_id.split(".")[0],temp_id.split(".")[0] + "_MALE_{}.jpg".format(i))
             core.run(run_args)
             male_list.append(cv2.imread(run_args.output_path))
 
         for i in range(1, 7):
             run_args.target_path = "/home/tobe1315/my_projects/Face_swapper/Background/Background/test_{}.jpg".format(i)
-            run_args.output_path = "/home/tobe1315/my_projects/Face_swapper/output/{}".format(temp_id.split(".")[0] + "_FEMALE_{}.jpg".format(i))
+            run_args.output_path = "/home/tobe1315/my_projects/Face_swapper/output/{}/{}".format(temp_id.split(".")[0],temp_id.split(".")[0] + "_FEMALE_{}.jpg".format(i))
             core.run(run_args)
             female_list.append(cv2.imread(run_args.output_path))
-        return male_list, female_list
+        return output_list
 
     
--- roop/roop/test.py
+++ roop/roop/test.py
@@ -1,6 +1,6 @@
 from fake_generator import *
 import cv2
 
-temp_image = cv2.imread("/home/tobe1315/my_projects/Face_swapper/source/sample1.png")
+temp_image = cv2.imread("/home/tobe1315/my_projects/Face_swapper/source/IMG_3911.JPG")
 print(temp_image)
 temp = generate_fake("F" ,temp_image)
